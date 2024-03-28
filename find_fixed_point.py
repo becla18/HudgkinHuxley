added jacobian eigenvalues computation and script to find fixed point

find_fixed_point.py
@@ -1,23 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import root
 from hudgkin_huxley import hudgkin_huxley_eqs
 
 
-# set I_ext values and initial optimization state
-I_ext_list = np.linspace(0.2, 0.4, 100)
-init_optimization_state = [-60, 0, 0, 0]
-const_params = [55, 40, -77, 35, -65, 0.3, 1]
+def find_fixed_point(I_ext_list, const_params):
+    # set I_ext values and initial optimization state
+    init_optimization_state = [-50, 0, 0, 0]
+    const_params = [55, 40, -77, 35, -65, 0.3, 1]
 
-# find fixed point for each I_ext
-fixed_points = []
-for I_ext in I_ext_list:
-    args = (I_ext, const_params)
-    solution = root(hudgkin_huxley_eqs, init_optimization_state, args)
-    print('success:', solution.success)
-    fixed_points.append(solution.x)
+    # find fixed point for each I_ext
+    fixed_points = []
+    for I_ext in I_ext_list:
+        args = (I_ext, const_params)
+        solution = root(hudgkin_huxley_eqs, init_optimization_state, args)
+        print('success:', solution.success)
+        fixed_points.append(solution.x)
 
-print(fixed_points)
+    return fixed_points
 
-plt.plot(I_ext_list, np.array(fixed_points)[:, 0])
-plt.show()

main_RK54.py
@@ -5,7 +5,7 @@ from scipy.integrate import solve_ivp
 from hudgkin_huxley import hudgkin_huxley_eqs
 
 
-I_ext_list = np.arange(-0.4, 1.4, 0.2)
+I_ext_list = np.arange(0.35, 0.39, 0.01)
 const_params = [55, 40, -77, 35, -65, 0.3, 1]
 init_state = [-60, 0, 0, 0]
 t_bounds = (0, 500)
@@ -28,7 +28,7 @@ sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
 
 plt.figure(figsize=(7, 4), dpi=300)
 for i, I_ext in enumerate(I_ext_list[::-1]):
-    plt.plot(time, V_I[-i-1], label= '$I_{ext} = $' + f'{I_ext:.1f}', color=sm.to_rgba(I_ext))
+    plt.plot(time, V_I[-i-1], label= '$I_{ext} = $' + f'{I_ext:.2f}', color=sm.to_rgba(I_ext))
 plt.xlabel('t [ms]')
 plt.ylabel('V [mV]')
 plt.xlim(t_bounds[0], t_bounds[1])
@@ -37,3 +37,4 @@ plt.subplots_adjust(right=0.7)
 plt.legend(loc='upper right', bbox_to_anchor=(1.35, 1))
 plt.savefig('/Users/benja/Desktop/HH_V_pour_differents_I.png')
 plt.show()
+